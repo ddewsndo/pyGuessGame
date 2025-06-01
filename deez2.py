ccc905d7d660055f526b7a8d961f8f5c59d226a3
ddewsndo <drewsando73@gmail.com>
2025-06-01 13:49:50 -0400

    update

--- deez2.py
+++ deez2.py
@@ -4,11 +4,18 @@
 #To save and push code^
 #clear screen only works for windows 
 
-#add easy mode that will give hints based on user input!!!
+
 #add menu and make it pretty
-#make the numbers genuinely random
+#add failsafes for input
+
+
 import random
 import os
+
+os.system('cls')
+
+print("This is a random number guesser, you have ranges to guess from and three lives per level. Guess it correct and move on!")
+
 level = 0
 listOne = [1, 2, 3, 4, 5]
 listTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -21,15 +28,17 @@ class guessGame:
         self.lives = lives
     def incorrect(self, userNum):
         os.system('cls')
+        self.levelStatement()
         self.lives-= 1
         self.hint(userNum)
-        print(f"You have {self.lives} left.")
+        print(f"You have {self.lives} lives left.\n")
     def correct (self):
         os.system('cls')
         #Should get rid of global level and level +=1 here, return it as true and use outside function to update level
         global level
-        print ("GOOD JOB!!!!")
+        print ("\nGOOD JOB!!!!")
         level += 1
+        self.levelStatement()
     def guess(self):
         userGuess = int(input("Enter Guess!   "))
         if userGuess == self.correctNum:
@@ -41,25 +50,33 @@ class guessGame:
             print("Too low...")
         if userNum > self.correctNum:
             print("Too high...")
-
-        
+    def levelStatement(self):
+        if level == 0:
+            print("\nLevel 1: Numbers range from 1 to 5, inclusive.\n")
+        elif level == 1:
+            print("\nLevel 2: Numbers range from 1 to 10, inclusive.\n")
+        elif level == 2:
+            print("\nLevel 3: Numbers range from 1 to 15, inclusive. Good luck!\n") 
+        elif level == 3:
+            print("\nLast Level! Numbers range from 1 through 10 again, however only one life now instead of three. Choose carefully...\n")
 
 levelOne = guessGame(listOne, random.randint(1, 5), 3)
 levelTwo = guessGame(listTwo, random.randint(1, 10), 3)
 levelThree = guessGame(listThree, random.randint(1, 15), 3)
 levelFour = guessGame(listTwo, random.randint(1 , 10), 1)
 
+levelOne.levelStatement()
+
 while level == 0 and levelOne.lives > 0:
-    print("Level 1: Numbers range from 1 to 5, inclusive.")
     levelOne.guess()
 while level == 1 and levelTwo.lives > 0:
-    print("Level 3: Numbers range from 1 to 10, inclusive.")
     levelTwo.guess()
 while level == 2 and levelThree.lives > 0:
-    print("Level 3: Numbers range from 1 to 15, inclusive. Good luck!")
     levelThree.guess()
 while level == 3 and levelFour.lives > 0:
-    print("Last Level! Back to range being 1 through 10, however only one life now instead of three. Choose carefully...")
     levelFour.guess()
 if levelOne.lives == 0 or levelTwo.lives == 0 or levelThree.lives == 0 or levelFour.lives == 0:
-    print("YOU LOSTTTTT")
+    os.system('cls')
+    print("\n\nYOU LOSTTTTT\n\n")
+if level == 4:
+    print("\n\nY O U  F R E A K I N G  W O N ! ! ! ! ! !\n\n")
